63d074ad1cdde51050d4ab73014e3da9d9e449a8
Adrian Mucha <kestarumper8@gmail.com>
2020-10-09 22:23:37 +0200

    Working WebRTC client alpha.

--- client/src/hooks/useWebRTC.ts
+++ client/src/hooks/useWebRTC.ts
@@ -1,16 +1,29 @@
 import Peer from "peerjs";
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect, useMemo, useCallback } from "react";
 
-const STUN_URL = "stun:stun1.l.google.com:19302";
+const STUN_URLS = [
+  "stun:stun.l.google.com:19302",
+  "stun:stun1.l.google.com:19302",
+  "stun:stun2.l.google.com:19302",
+  "stun:stun3.l.google.com:19302",
+  "stun:stun4.l.google.com:19302",
+];
 
 type HookReturn = {
+  error?: Record<string, unknown>;
   id: string;
   loading: boolean;
-  error?: string;
+  connectTo: (id: string) => Promise<Peer.MediaConnection>;
 };
 
-export default function useWebRTC(): HookReturn {
-  const [state, setId] = useState<HookReturn>({
+function getStream() {
+  return navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+}
+
+export default function useWebRTC(targetId: string): HookReturn {
+  const [state, setState] = useState<
+    Pick<HookReturn, "id" | "loading" | "error">
+  >({
     id: null,
     loading: true,
   });
@@ -21,23 +34,51 @@ export default function useWebRTC(): HookReturn {
     () =>
       new Peer({
         config: {
-          iceServers: [{ urls: STUN_URL }],
+          iceServers: [{ urls: STUN_URLS }],
         },
       }),
     []
   );
 
+  const connectTo = useCallback(
+    (destId: string) => getStream().then((stream) => peer.call(destId, stream)),
+    []
+  );
+
   useEffect(() => {
     peer.on("open", (newId) => {
-      setId({ id: newId, loading: false });
+      setState({ ...state, id: newId, loading: false });
+      console.log("open", newId);
     });
 
-    peer.on("error", (err) => setId({ ...state, loading: false, error: err }));
+    peer.on("error", (err) => {
+      setState({ ...state, loading: false, error: err.message });
+      console.error(err);
+    });
+
+    peer.on("call", async (call: Peer.MediaConnection) => {
+      console.log("call", call);
+      const stream = await getStream();
+      const video = document.getElementById(targetId) as HTMLVideoElement;
+
+      video.style.border = "1px solid black";
+
+      call.answer(stream);
+
+      call.on("stream", (remoteStream) => {
+        try {
+          video.srcObject = stream;
+        } catch (error) {
+          video.src = window.URL.createObjectURL(stream);
+        }
+        console.log("onStream", remoteStream);
+      });
+    });
 
     return () => {
       peer.destroy();
     };
   }, []);
 
-  return { id, loading, error };
+  return { id, loading, error, connectTo };
 }
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import useWebRTC from "./hooks/useWebRTC";
 
+function Loading() {
+  return <h1>Loading...</h1>;
+}
+
+function Error({ error }: { error: Record<string, unknown> }) {
+  return <pre>{JSON.stringify(error, null, 2)}</pre>;
+}
+
 function App() {
-  const state = useWebRTC();
+  const [targetId, setTargetId] = useState("");
+  const { id, loading, error, connectTo } = useWebRTC("vid");
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <Error error={error} />;
+  }
+
   return (
     <>
-      <h1>Hello Worlddddd</h1>
-      <pre>{JSON.stringify(state, null, 2)}</pre>
+      <h1>Your peerId: {id}</h1>
+      <input
+        type="text"
+        onChange={(e) => setTargetId(e.target.value)}
+        value={targetId}
+      />
+      <button
+        onClick={() =>
+          connectTo(targetId).catch(alert)
+        }
+      >
+        Call {targetId}
+      </button>
     </>
   );
 }
